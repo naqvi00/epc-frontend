54f9dc487a7e90db457ced8ca099cc47c812dfc3
naqvi00 <rnaqvi00@gmail.com>
2026-03-31 00:15:43 +0500

    Create PartnersLogo.tsx

---
+++ src/components/home/PartnersLogo.tsx
@@ -0,0 +1,9 @@
+import partner1 from '../../assets/3 partner.jpeg'
+import partner2 from '../../assets/new media.jpeg'
+import partner3 from '../../assets/Eurasianstar.jpeg'
+
+export const partners = [
+  { name: "Partner 1", logo: partner1 },
+  { name: "Partner 2", logo: partner2 },
+  { name: "Partner 3", logo: partner3 },
+]
