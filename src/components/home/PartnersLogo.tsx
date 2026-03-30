f12e8b6f451db93060ef94ae6af7b1798361a0ca
naqvi00 <rnaqvi00@gmail.com>
2026-03-31 00:28:29 +0500

    Update PartnersLogo.tsx

--- src/components/home/PartnersLogo.tsx
+++ src/components/home/PartnersLogo.tsx
@@ -1,7 +1,6 @@
 import partner1 from '../../assets/3 partner.jpeg'
 import partner2 from '../../assets/new media.jpeg'
 import partner3 from '../../assets/Eurasianstar.jpeg'
-
 export const partners = [
   { name: "Partner 1", logo: partner1 },
   { name: "Partner 2", logo: partner2 },
